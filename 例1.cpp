d8e85a40cee13d096ecc1342c995c58297f3a64d
tuwenlong1918 <2363996834@qq.com>
2019-03-30 20:08:10 +0800

    add update

---
+++ 例1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>                         //：有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
+int main()
+{
+	int i,j,k,m;
+	for(i=1,i<5,i++)
+	{for(j=1,j<5,j++)
+	{
+	for(k=1,k<5,k++){
+	if(i!=k&&i!=j&&j!=k){
+	printf("%d,%d,%d",i,j,k);
+}
+}
+}
+}
+
+	
+	
+}
