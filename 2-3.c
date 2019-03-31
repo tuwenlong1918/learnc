3c695260403d7bdc1d183e6005ca9121b26593e8
tuwenlong1918 <2363996834@qq.com>
2019-03-31 13:28:59 +0800

    change function

--- 2-3.c
+++ 2-3.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 int main()
 {
-	int p,i;
-	i=2,p=1;
-   while (i<=5)
+	int p = rev(6);
+	printf("%d",p);
+	getchar();
+ } 
+
+ int rev(int iNumber){
+    int p=1;
+	int i=1;
+    while (i<= iNumber)
     {
     	p=p*i;
-    	i=i+1;
+    	i++;
 	}
-	printf("%d",p);
- } 
+	return p;
+ }
